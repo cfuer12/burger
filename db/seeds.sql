a34c4211a624eb9a8b208b82a56a4f076b4d75f2
Carl <cfuer12@yahoo.com>
2021-02-14 21:55:16 -0800

    create db, serverJS and connectionJS files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Baconator', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Style', true);
