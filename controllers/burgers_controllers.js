af0a23f8dbeb839ca1c8eca6ee366f9fb758d061
Carl <cfuer12@yahoo.com>
2021-02-18 23:38:17 -0800

    orm file update

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
@@ -8,11 +7,11 @@ const burger = require('../models/burger');
 // Create all our routes and set up logic within those routes where required.
 router.get('/', (req, res) => {
   burger.selectAll((data) => {
-    const handlebarsObject = {
+    const hbsObject = {
       burgers: data,
     };
-    console.log(handlebarsObject);
-    res.render('index', handlebarsObject);
+    console.log(hbsObject);
+    res.render('index', hbsObject);
   });
 });
 
@@ -26,7 +25,7 @@ router.post('/api/burger', (req, res) => {
 router.put('/api/burger/:id', (req, res) => {
   const condition = `id = ${req.params.id}`;
 
-  console.log('condition', condition);
+  // console.log('condition', condition);
 
   burger.updateOne(
     {
